21b69fe6af5238389135f6e7f902355abd0c948f
Ana Westphalen <campos1984@hotmail.com>
2022-02-06 19:43:09 -0300

    criação de controller e rotas

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      resources :events, only: %i[index show update]
+    end
+  end
   root to: 'pages#home'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
